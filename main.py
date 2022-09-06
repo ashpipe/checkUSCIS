69de3d69726dbcf657794286419fbebe63b16d87
EC2 Default User <ec2-user@ip-172-31-46-132.us-east-2.compute.internal>
2022-09-06 21:27:07 +0000

    delete unused packages

--- main.py
+++ main.py
@@ -1,17 +1,11 @@
-import os
-import sys
-
-#sys.path.append(f"{os.path.dirname(__file__)}/../../api")
 import gmail
 import uscis
-import json
-import copy
 
 
 def main():
     msg = uscis.requestStatus(uscis.case_num)
     if msg != uscis.old_message:
-        gmail.send_strmsg(msg)
+        gmail.send_strmsg(title="Python: USCIS update", msg=msg)
 
 
 if __name__ == "__main__":
