acc2cb5fb4409bc5ef1d3a1dd794fe5bc5e2c739
EC2 Default User <ec2-user@ip-172-31-46-132.us-east-2.compute.internal>
2022-09-08 12:12:34 +0000

    added test function

--- src/app.py
+++ src/app.py
@@ -6,10 +6,14 @@ def main():
     msg = uscis.requestStatus(uscis.case_num)
     if msg != uscis.old_message:
         gmail.send_strmsg(title="Python: USCIS update", msg=msg)
+    return msg
 
 
 def handler(event, context):
-    main()
+    msg = main()
+
+    if event["key1"] == "test":
+        gmail.send_strmsg(title="Python: USCIS update: test", msg=msg)
 
 
 if __name__ == "__main__":
